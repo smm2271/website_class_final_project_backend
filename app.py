3fe1ae62030e294aafe59685523a81942e05d970
sumou <zalicsu@gmail.com>
2025-11-04 15:31:30 +0800

    'init'

---
+++ app.py
@@ -0,0 +1,11 @@
+import fastapi
+import uvicorn
+
+app = fastapi.FastAPI()
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
